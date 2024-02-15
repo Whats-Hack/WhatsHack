6466f46149fa1648109c42e560d4149bc77dead1
chyVacheck <dmytro.shah.work@gmail.com>
2024-02-15 16:21:37 +0100

    feat: backend add sign lohic
    - create login function and router
    - create register function and router
    - change model of user a bit
    - protect router by token (jwt)
    
    Co-Authored-By: Hugo Maurin <148004075+Huguiz@users.noreply.github.com>

--- backend/README.md
+++ backend/README.md
@@ -26,28 +26,30 @@
   request
 
   ```json
-    {
-      "username": "example", // required
-      "password": "example pass", // required
-      "avatar": "url",
-      "email": "example@text.com",
-      "firstName": "example firstName",
-      "lastName": "example lastName",
-      "birthday": Date, // type is a date
-      "city": "example city",
-    }
+  {
+    "username": "example", // required
+    "password": "example pass", // required
+    "avatar": "url",
+    "email": "example@text.com",
+    "firstName": "example firstName",
+    "lastName": "example lastName",
+    "birthday": "2024-02-15T15:15:24.947Z", // type is a date
+    "city": "example city"
+  }
   ```
 
   answer
 
   ```json
   {
-    {
+    "data": {
       "id": "id", // uniq // don't be a changeably
-      "creationDate": Date, // type is a date
+      "creationDate": "2024-02-15T15:15:24.947Z", // type is a date
       "token": "some token", // id: iehbvpehwifbvpehf -> 12345
-      "lastConnection":  Date, // type is a date
+      "lastConnection": "2024-02-15T15:15:24.947Z", // type is a date
       "friends": [],
+      "chats": [],
+      "isActive": true,
 
       "email": "example@text.com",
       "avatar": "url",
@@ -57,6 +59,8 @@
       "lastName": "example lastName",
       "birthday": Date, // type is a date
       "city": "example city"
-    }
+    },
+    "message": "User is created",
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzA4MDEwMTI0fQ.x7xF2v_hUwKd1GNslgktRjV_nwfqp_Il8IJnhiGeILU"
   }
   ```
--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 // todo write a cool backend application
 
 require("dotenv").config();
+const express = require("express");
 const jsonServer = require("json-server");
+const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 
+// importing the fs module
+const fs = require("fs");
+const db = require("./databases/db.json");
+
 const server = jsonServer.create();
-
 const router = jsonServer.router("./databases/db.json");
-
 const middlewares = jsonServer.defaults();
+
+const secretKey = process.env.SECRET_KEY || "your_secret_key"; // Секретный ключ для подписи JWT токена
 const PORT = process.env.PORT || 5000;
 
+server.use(express.json());
 server.use(middlewares);
 server.use(morgan("dev"));
 server.use((req, res, next) => {
@@ -19,7 +26,123 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use(router);
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader;
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+function login(req, res, next) {
+  const { userName, password } = req.body;
+
+  for (let i = 0; i < db.users.length; i++) {
+    const user = db.users[i];
+    if (user.username === userName) {
+      if (user.password === password) {
+        const token = jwt.sign(
+          {
+            id: user.id,
+          },
+          secretKey
+        );
+        res.json({ token: token });
+      } else {
+        res.status(400);
+        res.json({ error: "Password are wrong" });
+      }
+      return;
+    }
+  }
+
+  res.status(404);
+  res.json({ error: "User not fround" });
+}
+
+function register(req, res, next) {
+  const {
+    email,
+    avatar,
+    username,
+    password,
+    firstName,
+    lastName,
+    birthday,
+    city,
+  } = req.body;
+
+  if (!username || !password) {
+    res.status(400);
+    return res.json({ error: "Username and password are required" });
+  }
+
+  // validation for uniq username
+  for (let i = 0; i < db.users.length; i++) {
+    const user = db.users[i];
+    if (user.username === username) {
+      res.status(400);
+      return res.json({ error: "This username is already chosen" });
+    }
+  }
+
+  const _id = db.users.length;
+
+  const newUser = {
+    id: _id,
+    creationDate: new Date(),
+    lastConnection: new Date(),
+    friends: [],
+    chats: [],
+    email: email || "",
+    avatar: avatar || "",
+    username: username,
+    password: password,
+    firstName: firstName || "",
+    lastName: lastName || "",
+    birthday: birthday || "",
+    city: city || "",
+    isActive: true,
+  };
+
+  // add a new one user
+  db.users.push(newUser);
+
+  // converting the JSON object to a string
+  const data = JSON.stringify(db);
+
+  // writing the JSON string content to a file
+  fs.writeFile("./databases/db.json", data, (error) => {
+    // throwing the error
+    // in case of a writing problem
+    if (error) {
+      // logging the error
+      res.status(500);
+      return res.json({ error: error });
+    }
+
+    return res.json({
+      data: newUser,
+      message: "User is created",
+      token: jwt.sign(
+        {
+          id: _id,
+        },
+        secretKey
+      ),
+    });
+  });
+}
+
+server.post("/api/login", login);
+
+server.post("/api/register", register);
+
+server.use("/api", authenticateToken, router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
