361e3ddcb7f102d6b9b68cf1f32903a8c37f0fc3
chyVacheck <dmytro.shah.work@gmail.com>
2024-02-19 11:19:35 +0100

    fix: change some css rules to fix
    - fixed chats
    - fixed chat (hover + active)
    
    Co-Authored-By: Hugo Maurin <148004075+Huguiz@users.noreply.github.com>

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -12,43 +12,49 @@ import logout from './../assets/logout.png';
 import './NavBar.css';
 
 export default function NavBar({ setLogged, setCurrentUser }) {
-    function logoutUser() {
-        setCurrentUser({
-            email: "",
-            avatar: "",
-            username: "",
-            password: "",
-            firstName: "",
-            lastName: "",
-            birthday: "",
-            city: "",
-        });
+  function logoutUser() {
+    setCurrentUser({
+      email: '',
+      avatar: '',
+      username: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      birthday: '',
+      city: '',
+    });
 
-        setLogged(false)
-
-
-    }
-    return (
-        <div className='navbar_container'>
-            <Link to={'/chat'}>
-                <div className='navbar_image_container'>
-                    <img className='navbar_image' src={chatImg} alt='ChatImg' />
-                </div>
-            </Link>
-            <div className='navbar_image_container'>
-                <img className='navbar_image' src={addUserImg} alt='ChatImg' />
-            </div>
-            <div className='navbar_image_container'>
-                <img className='navbar_image' src={param} alt='ChatImg' />
-            </div>
-            <Link to={'/about'}>
-                <div className='navbar_image_container'>
-                    <img className='navbar_image' src={about} alt='ChatImg' />
-                </div>
-            </Link>
-            <div className='navbar_image_container logout_image' onClick={logoutUser}>
-                <img className='navbar_image' src={logout} alt='ChatImg' />
-            </div>
+    setLogged(false);
+  }
+  return (
+    <div className='navbar_container'>
+      <Link to={'/chat'}>
+        <div className='navbar_image_container'>
+          <img className='navbar_image' src={chatImg} alt='ChatImg' />
         </div>
-    );
+      </Link>
+
+      <Link to={'/friends'}>
+        <div className='navbar_image_container'>
+          <img className='navbar_image' src={addUserImg} alt='ChatImg' />
+        </div>
+      </Link>
+
+      <Link to={'/settings'}>
+        <div className='navbar_image_container'>
+          <img className='navbar_image' src={param} alt='ChatImg' />
+        </div>
+      </Link>
+
+      <Link to={'/about'}>
+        <div className='navbar_image_container'>
+          <img className='navbar_image' src={about} alt='ChatImg' />
+        </div>
+      </Link>
+
+      <div className='navbar_image_container logout_image' onClick={logoutUser}>
+        <img className='navbar_image' src={logout} alt='ChatImg' />
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/NavChats.jsx
+++ frontend/src/components/NavChats.jsx
@@ -1,106 +1,154 @@
-import "./NavChats.css"
-import arrowImg from "./../assets/arrow.png"
+import './NavChats.css';
+import arrowImg from './../assets/arrow.png';
 
 export default function NavChats() {
-    return (
-        <>
-            <input type="checkbox" id="toggleButton"/>
-            <label for="toggleButton" id="sideButton"><img src={arrowImg} alt="arrow-image" /></label>
-            <div className="navchats_container" id="panel">
-                <div className="navchats_h2_container">
-                    <h2 className="navchats_h2">Message</h2>
-                </div>
-                <div className="navchats_container_limited">
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Luis Junco</p>
-                            <p className="navchats_text">Hey how are you my friend</p>
-                        </div>
-                        <p className="navchats_time">9:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Princess Peach</p>
-                            <p className="navchats_text">Heyyyyyyyyyyyyyyyyyyyyyyyy</p>
-                        </div>
-                        <p className="navchats_time">10:08</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://t3.ftcdn.net/jpg/04/60/91/88/360_F_460918802_XVCymFr7MoziFpnInbTDvrlblYhvAOi2.jpg" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Dima</p>
-                            <p className="navchats_text">Hey how are you my friend dqzdqzd Hey how are you my friend</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Test Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">13:58</p>
-                    </div>
-                    <div className="navchats_chat_container">
-                        <img className="navchats_avatar" src="https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM=" alt="avatar" />
-                        <div className="navchats_text_container">
-                            <p className="navchats_name">Last Name</p>
-                            <p className="navchats_text">Hey</p>
-                        </div>
-                        <p className="navchats_time">08:45</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <input type='checkbox' id='toggleButton' />
+      <label htmlFor='toggleButton' id='sideButton'>
+        <img src={arrowImg} alt='arrow-image' />
+      </label>
+      <div className='navchats_container' id='panel'>
+        <div className='navchats_h2_container'>
+          <h2 className='navchats_h2'>Message</h2>
+        </div>
+        <div className='navchats_container_limited'>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg'
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Luis Junco</p>
+              <p className='navchats_text'>Hey how are you my friend</p>
             </div>
-        </>
-    )
+            <p className='navchats_time'>9:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://a.storyblok.com/f/191576/1200x800/faa88c639f/round_profil_picture_before_.webp'
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Princess Peach</p>
+              <p className='navchats_text'>Heyyyyyyyyyyyyyyyyyyyyyyyy</p>
+            </div>
+            <p className='navchats_time'>10:08</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://t3.ftcdn.net/jpg/04/60/91/88/360_F_460918802_XVCymFr7MoziFpnInbTDvrlblYhvAOi2.jpg'
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Dima</p>
+              <p className='navchats_text'>
+                Hey how are you my friend dqzdqzd Hey how are you my friend
+              </p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Test Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>13:58</p>
+          </div>
+          <div className='navchats_chat_container'>
+            <img
+              className='navchats_avatar'
+              src='https://media.istockphoto.com/id/1317804578/es/foto/una-empresaria-dispar%C3%B3-con-la-cabeza-sonriendo-a-la-c%C3%A1mara.jpg?s=612x612&w=0&k=20&c=CLbDPD1RNphC0QAseQfMRN10s66SNDLh1iOVkXjCBUM='
+              alt='avatar'
+            />
+            <div className='navchats_text_container'>
+              <p className='navchats_name'>Last Name</p>
+              <p className='navchats_text'>Hey</p>
+            </div>
+            <p className='navchats_time'>08:45</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
