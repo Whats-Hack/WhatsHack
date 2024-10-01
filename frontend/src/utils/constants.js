b98e3232f6f242d2d1a815744cf12c7244de8aa9
Huguiz <hugo.maurin@hotmail.com>
2024-10-01 23:13:52 +0200

    Update backend URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -9,7 +9,7 @@ export const STATUS = {
 export const API_SETTING = {
   baseURL: STATUS.DEV
     ? 'http://localhost:5005/api'
-    : 'https://whatshack.adaptable.app/api',
+    : 'https://whats-hack.adaptable.app/api',
   contentType: 'application/json',
 };
 
